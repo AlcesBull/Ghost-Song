08332892fd9778f3f0c5527097fb7a8603c91a3f
AlcesBull <spencer@alcesbull.com>
2017-12-18 16:17:03 -0500

    refactored to work with Slack

--- index.js
+++ index.js
@@ -1,14 +1,44 @@
-//Testing out a new method for building Slackbots
+//Testing out a new method for building Slackbots in a local machine test env
 
-var http = require('http');
-const PORT=4390
+var express = require('express');
+var request = require('request');
 
-function handleRequest(request, response){
-	response.end('You set it up correctly! The path to your fixed purpose is paved with iron rails... - Path Hit: ' + request.url);
-}
+//variables obscured in env
+var clientID = process.env.clientID;
+var clientSECRET = process.env.clientSECRET;
 
-var server = http.createServer(handleRequest);
+var app = express();
 
-server.listen(PORT, function(){
-	console.log("Server listening on: http://localhost:%s", PORT);
+const PORT=4390;
+
+app.listen(PORT, function(){
+	console.log("Example app listening on port " + PORT);
+});
+
+app.get('/', function(req, res) {
+	res.sent('The path to your fixed purpose is paved with iron rails! Path Hit: ' + req.url);
+});
+
+app.get('/oauth', function(req, res) {
+	if (!req.query.code) {
+		res.status(500);
+		res.send({"Error": "Looks like we're not getting code."});
+		console.log("Looks like we're not getting code.");
+	} else {
+	request({
+		url: 'https://slack.com/api/oauth.access',
+		qs: {code: req.query.code, client_id: clientID, client_secret: clientSECRET},
+		method: 'GET',
+	}, function (error, response, body) {
+		if (error) {
+			console.log(error);
+		} else {
+			res.json(body);
+		}
+	})
+	}
+});
+
+app.post('/command', function(req, res) {
+	res.send('Swerve you?! The path to your fixed purpose is paved with iron rails!');
 });
